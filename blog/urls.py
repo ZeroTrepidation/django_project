727443f3524a7a2acef811461353155e051ac0dc
ZeroTrepidation <sethtummillo@yahoo.com>
2021-03-28 18:43:55 -0500

    The ability to delete, update, and create posts has been added. Also now allows someone to filter posts by user

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from .views import (PostListView, PostDetailView, UserPostListView,
+                    PostCreateView, PostUpdateView, PostDeleteView)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name="blog-about"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),      # <app>/<model>_form.html
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 
 ]
